1ba9c2448ce85d5be8a0e7dc3e32aa5858c8e98b
Shunza254 <shunzachillavert@gmail.com>
2024-11-06 22:12:45 +0300

    First Python project

---
+++ trials.py
@@ -0,0 +1,6 @@
+name = input("What is your name?\n")
+color = input("What is your favorite color?\n")
+
+print("Hello %s, your favorite color, %s, is awesome!"% (name, color))
+
+print(f"Hello {name} and your color is {color}")
---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Name: ")
+age = input("Age: ")
+location = input("Location: ")
+
+print(f"Hello {name}, you are {age} years of age and live in {location}.")
